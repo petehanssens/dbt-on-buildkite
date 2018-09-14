955e0283cada459d7177f77d0dd045af4c7af971
Peter Hanssens <phanssens1@gmail.com>
2018-09-14 14:52:29 +1000

    ready for first test run

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # dbt-on-buildkite
 running dbt from buildkite
+
+
+
+### putting secure string ssm parameters into aws using cli
+aws ssm put-parameter --name MyParameter --value "secret_value" --type SecureString
+
+
+
+### dbt models for [your org/project name]
+
+---
+- [What is dbt](https://dbt.readme.io/docs/overview)?
+- Read the [dbt viewpoint](https://dbt.readme.io/docs/viewpoint)
+- [Installation](https://dbt.readme.io/docs/installation)
+- Join the [chat](http://ac-slackin.herokuapp.com/) on Slack for live questions and support.
+
+---
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-password=$(aws ssm get-parameter --region ap-southeast-2 --name "/shared/REDSHIFT_PASSWORD" --with-decryption --query "Parameter.Value" | tr -d \")
+password=$(aws ssm get-parameter --region ap-southeast-2 --name "/shared/redshift" --with-decryption --query "Parameter.Value" | tr -d \")
 sed --in-place "s/new_password/$password/" "/root/.dbt/profiles.yml"
 
 dbt run 
 dbt generate docs
-token=$(aws ssm get-parameter --region us-east-1 --name "/production/secrets/shared/NETLIFY_DEPLOY_TOKEN" --with-decryption --query "Parameter.Value" | tr -d \")
+token=$(aws ssm get-parameter --region us-east-1 --name "/shared/netlify" --with-decryption --query "Parameter.Value" | tr -d \")
 echo 'Running netlifyctl: '
 ./netlifyctl -A $token deploy
